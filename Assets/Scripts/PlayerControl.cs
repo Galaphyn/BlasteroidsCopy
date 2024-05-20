e6e4425289bd014332ce9adc925539e515fd8e0e
Galaphyn <rorymb10@gmail.com>
2024-05-20 12:25:56 -0700

    Shooting added

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private float speed = 5f;
+
+    public float projSpeed = 10f;
+    public GameObject projectile;
     // Start is called before the first frame update
     void Start()
     {
@@ -31,7 +34,19 @@ public class PlayerControl : MonoBehaviour
 
         if (Input.GetKey(KeyCode.Mouse0))
         {
-
+            ShootLaser(direction);
         }
+    }
+
+    private void ShootLaser(Vector2 direction)
+    {
+        //Instantiate and move projectile
+        GameObject proj = Instantiate(projectile, transform.position, Quaternion.identity);
+        proj.GetComponent<Rigidbody2D>().velocity = direction.normalized * projSpeed;
+
+        //Calculate angle of velocity to rotate laser
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        angle -= 90;
+        proj.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
