1bb5deaf4de37bfa3594440a12a4951491b92681
Galaphyn <rorymb10@gmail.com>
2024-05-20 02:56:09 -0700

    Githubfixes

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;
+
+        Vector3 pos = transform.position;
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos.x -= speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            pos.x += speed * Time.deltaTime;
+        }
+        transform.position = pos;
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+
+        }
+    }
+}
